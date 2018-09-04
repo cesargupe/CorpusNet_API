39237f6dfebc45d29183b99dc360e745fda4ef31
Cesar Gutierrez <cgutip00@estudiantes.unileon.es>
2018-09-04 11:53:04 +0200

    add corpus-paralelos json

--- mongodb/importDB.sh
+++ mongodb/importDB.sh
@@ -3,3 +3,4 @@ mongo corpusnet --eval 'db.contents.drop()'
 mongoimport --db corpusnet --collection contents --file general.json --jsonArray
 mongoimport --db corpusnet --collection contents --file inicio.json --jsonArray
 mongoimport --db corpusnet --collection contents --file corpus-comparables.json --jsonArray
+mongoimport --db corpusnet --collection contents --file corpus-paralelos.json --jsonArray
