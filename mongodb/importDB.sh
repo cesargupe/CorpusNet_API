8e5b010fa951d47970346505b7e958798158f238
Cesar Gutierrez <cgutip00@estudiantes.unileon.es>
2018-09-12 14:02:08 +0200

    complete json templates

--- mongodb/importDB.sh
+++ mongodb/importDB.sh
@@ -4,3 +4,9 @@ mongoimport --db corpusnet --collection contents --file general.json --jsonArray
 mongoimport --db corpusnet --collection contents --file inicio.json --jsonArray
 mongoimport --db corpusnet --collection contents --file corpus-comparables.json --jsonArray
 mongoimport --db corpusnet --collection contents --file corpus-paralelos.json --jsonArray
+mongoimport --db corpusnet --collection contents --file herramientas-tagsets.json --jsonArray
+mongoimport --db corpusnet --collection contents --file herramientas-tecnicas.json --jsonArray
+mongoimport --db corpusnet --collection contents --file aplicaciones.json --jsonArray
+mongoimport --db corpusnet --collection contents --file grupos.json --jsonArray
+mongoimport --db corpusnet --collection contents --file noticias.json --jsonArray
+npm start
